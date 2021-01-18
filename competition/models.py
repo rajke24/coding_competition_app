a6a7a581598a47a0922eadcf8dcb45e552c34994
Jacek <jacek.szelewski@gmail.com>
2021-01-18 09:25:17 +0100

    Add models

--- competition/models.py
+++ competition/models.py
@@ -1,3 +1,49 @@
+
+
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    description = models.CharField(max_length=1000)
+
+
+class Test(models.Model):
+    input = models.CharField(max_length=500)
+    output = models.CharField(max_length=500)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+
+class Solution(models.Model):
+    class SolutionStatus(models.IntegerChoices):
+        NOT_EVALUATED = 1
+        CORRECT = 2
+        INCORRECT = 3
+        PRESENTATION_ERROR = 4
+        COMPILATION_ERROR = 5
+        RUNTIME_ERROR = 6
+        TIME_EXCEEDED_ERROR = 7
+
+    content = models.CharField(max_length=2048)
+    upload_time = models.DateTimeField()
+    solution_status = models.IntegerField(choices=SolutionStatus.choices)
+
+
+class SolutionTestResult(models.Model):
+    did_pass = models.BooleanField()
+    solution = models.ForeignKey(Solution, on_delete=models.CASCADE)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+
+
+class Configuration(models.Model):
+    class CompetitionStatus(models.IntegerChoices):
+        INACTIVE = 0
+        ACTIVE = 1
+
+    class RankingVisibility(models.IntegerChoices):
+        INVISIBLE = 0
+        VISIBLE = 1
+
+    participants_limit = models.PositiveIntegerField()
+    competition_status = models.IntegerField(choices=CompetitionStatus.choices)
+    ranking_visibility = models.IntegerField(choices=RankingVisibility.choices)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
+class Team(models.Model):
+    team_as_user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="Zespół-użytkownik")
+    is_disqualified = models.BooleanField(default=False, verbose_name="Czy zdyskwalifkowany")
+    guardian_first_name = models.CharField(max_length=64, blank=True, verbose_name="Imię opiekuna")
+    guardian_last_name = models.CharField(max_length=64, blank=True, verbose_name="Nazwisko opiekuna")
+    school_name = models.CharField(max_length=100, verbose_name="Nazwa szkoły")
+
+    def __str__(self):
+        return self.team_as_user.username
+
+
+class Participant(models.Model):
+    email = models.EmailField(primary_key=True, verbose_name="Adres e-mail")
+    first_name = models.CharField(max_length=64, verbose_name="Imię")
+    last_name = models.CharField(max_length=64, verbose_name="Nazwisko")
+    age = models.IntegerField(verbose_name="Wiek", validators=[
+        MinValueValidator(14, "Wiek musi być pomiędzy 14 a 99"),
+        MaxValueValidator(99, "Wiek musi być pomiędzy 14 a 99")
+    ])
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, verbose_name="Zespół")
