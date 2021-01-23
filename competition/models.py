61a3154d8e60a7bf7f91152873ba67f91cd3295a
Jacek <jacek.szelewski@gmail.com>
2021-01-23 12:51:40 +0100

    Add evaluation of solutions

--- competition/models.py
+++ competition/models.py
@@ -4,6 +4,9 @@ from django.db import models
 
 
 # Create your models here.
+from users.models import Team
+
+
 class Task(models.Model):
     description = models.CharField(max_length=1000)
 
@@ -16,17 +19,19 @@ class Test(models.Model):
 
 class Solution(models.Model):
     class SolutionStatus(models.IntegerChoices):
-        NOT_EVALUATED = 1
-        CORRECT = 2
-        INCORRECT = 3
-        PRESENTATION_ERROR = 4
-        COMPILATION_ERROR = 5
-        RUNTIME_ERROR = 6
-        TIME_EXCEEDED_ERROR = 7
+        NOT_EVALUATED = 1, 'Nieocenione'
+        CORRECT = 2, 'Poprawne'
+        INCORRECT = 3, 'Niepoprawne'
+        PRESENTATION_ERROR = 4, 'Błąd prezentacji'
+        COMPILATION_ERROR = 5, 'Błąd kompilacji'
+        RUNTIME_ERROR = 6, 'Błąd czasu wykonania'
+        TIME_EXCEEDED_ERROR = 7, 'Przekroczono czas wykonania'
 
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
     content = models.CharField(max_length=2048)
-    upload_time = models.DateTimeField()
-    solution_status = models.IntegerField(choices=SolutionStatus.choices)
+    upload_time = models.DateTimeField(auto_now_add=True)
+    solution_status = models.IntegerField(choices=SolutionStatus.choices, default=SolutionStatus.NOT_EVALUATED)
 
 
 class SolutionTestResult(models.Model):
--- competition/views.py
+++ competition/views.py
@@ -1,10 +1,14 @@
+import subprocess
+from os import path
+import os
+
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from competition.forms import ParticipantForm, TeamForm, ConfigPanelForm, SolutionForm
 import random
-
-from competition.models import Configuration
+from enum import Enum
+from competition.models import Configuration, Task, Test, Solution, SolutionTestResult
 from users.models import Team, Participant
 
 
@@ -129,5 +133,101 @@ def __recreate_empty_forms(participant_forms, team_members):
 
 def send_solution(request):
     if request.method == 'POST':
-        return HttpResponse(request.POST['solution'])
+        task = Task.objects.all()[0]  # placeholder
+        team = Team.objects.all()[0]  # placeholder
+
+        solution = Solution.objects.create(task=task, team=team,
+                                           content=__sanitize_solution_content(request.POST['solution']))
+        solution_filename = __save_solution_to_file(solution)
+        solution_status = __run_tests(solution_filename, task, solution)
+        __delete_solution_file(solution_filename)
+        solution.solution_status = solution_status
+        solution.save()
+        return HttpResponse(solution_status.name)
     return render(request, 'competition/send_solution.html', context={'solution_form': SolutionForm()})
+
+
+def __sanitize_solution_content(content):
+    return '\n'.join(content.splitlines())
+
+
+def __save_solution_to_file(solution):
+    filename = __generate_filename()
+    file = open(filename, 'w')
+    file.write(solution.content)
+    file.close()
+    return filename
+
+
+def __generate_filename():
+    file_suffix = random.randint(1, 1000)
+    while True:
+        filename = "user_script{0}.py".format(file_suffix)
+        if not path.exists(filename):
+            return filename
+        file_suffix += 1
+
+
+def __run_tests(solution_script_path, task, solution):
+    tests = Test.objects.all().filter(task=task)
+    test_results = []
+    for test in tests:
+        test_result = __run_test(solution_script_path, test.input, test.output)
+        did_pass = test_result == TestResult.OKAY
+        SolutionTestResult.objects.create(solution=solution, test=test, did_pass=did_pass)
+        test_results.append(test_result)
+
+    for test_result in test_results:
+        if test_result is TestResult.TIME_EXCEEDED_ERROR:
+            return Solution.SolutionStatus.TIME_EXCEEDED_ERROR
+        elif test_result is TestResult.RUNTIME_ERROR:
+            return Solution.SolutionStatus.RUNTIME_ERROR
+        elif test_result is TestResult.COMPILATION_ERROR:
+            return Solution.SolutionStatus.COMPILATION_ERROR
+        elif test_result is TestResult.PRESENTATION_ERROR:
+            return Solution.SolutionStatus.PRESENTATION_ERROR
+        elif test_result is TestResult.WRONG_ANSWER:
+            return Solution.SolutionStatus.INCORRECT
+    return Solution.SolutionStatus.CORRECT
+
+
+def __run_test(script_path, test_input, test_output):
+    test_input_sanitized = test_input.replace('\\n', '\n')
+    test_output_sanitized = test_output.replace('\\n', '\n')
+    try:
+        result = subprocess.run(['python', script_path], input=test_input_sanitized, capture_output=True,
+                                encoding='utf-8', check=True)
+        is_output_correct = (test_output_sanitized == result.stdout.rstrip())
+        if is_output_correct:
+            return TestResult.OKAY
+        if __is_presentation_error(result.stdout, test_output_sanitized):
+            return TestResult.PRESENTATION_ERROR
+        return TestResult.WRONG_ANSWER
+    except subprocess.CalledProcessError as err:
+        return __evaluate_error(err.stderr)
+
+
+def __is_presentation_error(actual_output, expected_output):
+    actual_output_stripped = "".join(actual_output.split())
+    expected_output_stripped = "".join(expected_output.split())
+    return actual_output_stripped == expected_output_stripped
+
+
+def __evaluate_error(error_message):
+    if 'SyntaxError' in error_message:
+        return TestResult.COMPILATION_ERROR
+    return TestResult.RUNTIME_ERROR
+
+
+def __delete_solution_file(filepath):
+    if os.path.exists(filepath):
+        os.remove(filepath)
+
+
+class TestResult(Enum):
+    OKAY = 1
+    WRONG_ANSWER = 2
+    PRESENTATION_ERROR = 3
+    COMPILATION_ERROR = 4
+    RUNTIME_ERROR = 5
+    TIME_EXCEEDED_ERROR = 6
