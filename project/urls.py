0610894fee68b871cfed61f84a28df35a07e6fa0
rajke24 <jarek.andrzejewski99@gmail.com>
2021-01-23 20:42:57 +0100

    Move users functionality to users app

--- project/urls.py
+++ project/urls.py
@@ -2,14 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from competition.views import configpanel, ranking
-from competition import views
+from competition.views import configpanel, ranking, send_solution
+from users.views import register, no_team_slots_available
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('config-panel/', configpanel, name='config-panel'),
-    path('register/', views.register, name='registration'),
-    path('register/limit', views.no_team_slots_available, name='register-no-team-slots-available'),
-    path('solution', views.send_solution, name='send-solution'),
+    path('register/', register, name='registration'),
+    path('register/limit', no_team_slots_available, name='register-no-team-slots-available'),
+    path('solution', send_solution, name='send-solution'),
     path('ranking/', ranking, name='ranking'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
